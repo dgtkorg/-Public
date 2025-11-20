e00357dd3dca54c74882b09ed6b528e0af8f2eea
dgtkorg <musicnox@qq.com>
2025-11-20 16:11:37 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,25 @@
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Stats from '../components/Stats'
-import Wh
+import Features from '../components/Features'
+import VisionMission from '../components/VisionMission'
+import Tokenomics from '../components/Tokenomics'
+import Community from '../components/Community'
+import Roadmap from '../components/Roadmap'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <Header />
+      <Hero />
+      <Stats />
+      <Features />
+      <VisionMission />
+      <Tokenomics />
+      <Community />
+      <Roadmap />
+      <Footer />
+    </div>
+  )
+}
