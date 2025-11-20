521d5132a4dc91b527cec1aab13a5a665698baf2
dgtkorg <musicnox@qq.com>
2025-11-20 13:34:51 +0800

    Create main App component with routing

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+```tsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { LanguageProvider } from './lib/LanguageContext'
+import Home from './pages/Home'
+import Whitepaper from './pages/Whitepaper'
+
+function App() {
+  return (
+    <LanguageProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/whitepaper" element={<Whitepaper />} />
+        </Routes>
+      </Router>
+    </LanguageProvider>
+  )
+}
+
+export default App
+```
